b277066fe9d8336ca4fa3a4f4d98ce1db6d7360d
JennyZhu2 <jenz1236@gmail.com>
2024-11-20 12:29:53 -0500

    added learn more page for each mission

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import HomePage from "./assets/HomePage";
 import Header from "./assets/Header";
 import Footer from "./assets/Footer";
 import Login from "./pages/Login/login";
-import MapPage from "./pages/Map/map"; // Update this import
+import MapPage from "./pages/Map/map";
 import Dashboard from "./pages/UserDashboard/UserDashboard";
 import Post from "./pages/PostingPage/PostingPage";
 import Tours from "./pages/Tours/Tours"
 import TourView from "./pages/TourView/TourView"
 import TourView_withhunt from "./pages/TourView_withhunt/TourView";
+import Mission from "./pages/Map/mission";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/tours" element={<Tours />} />
           <Route path="/view/:tourId" element={<TourView />} />
           <Route path="/view_withhunt/:tourId" element={<TourView_withhunt />} />
+          <Route path="/tour/:tourId/mission/:missionStep" element={<Mission />} />
         </Routes>
         <Footer />
       </Router>
